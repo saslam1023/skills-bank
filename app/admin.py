8eb822c7e95b7eea43178b5a47e261f296d0c4ab
saslam1023 <YOUR_GITHUB_NO_REPLY_EMAIL>
2025-02-19 22:28:49 +0000

    updated folder content

--- app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Skill, Project, AIAnalysis, Portfolio, Category, Context
-
-admin.site.register(Category)
-admin.site.register(Context)
-admin.site.register(Skill)
-admin.site.register(Project)
-admin.site.register(AIAnalysis)
-admin.site.register(Portfolio)
